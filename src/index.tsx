d1300990065adf2c71cfcabfc9498556a9899f19
Mohankumar@LAPTOP-DSRRQ3HB <gmk9155@gmail.com>
2024-11-06 10:20:36 +0530

    second commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
-import { ChakraProvider} from "@chakra-ui/react";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
@@ -9,7 +9,7 @@ if (!container) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <ChakraProvider >
+  <ChakraProvider value={defaultSystem}>
     <App />
   </ChakraProvider>
 );
